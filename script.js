d20740e845b0e4b7a544df79933682ff94de4202
Alex <alex-fil@mail.ru>
2020-05-26 14:48:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".border").css("border","2px solid pink");
+    $(".border").css("display","inline-block");
+    $(".teltext").html("+7 (903) 11-11-133");
+    $(".teltext").css("fontSize", "10px");
+    $(".invisible").css("display", "none");
+    $(".greentext").css("color", "green");
+})
